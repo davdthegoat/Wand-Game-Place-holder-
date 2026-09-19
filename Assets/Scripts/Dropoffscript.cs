2148d2bbaaf478f565e1eaf1c592624f93e953a1
nether-porta <mibsamaarij09+github@gmail.com>
2026-09-19 23:40:18 +0500

    Added Script for dropping items into shelf

--- Assets/Scripts/Dropoffscript.cs
+++ Assets/Scripts/Dropoffscript.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PickUpScript : MonoBehaviour
+public class DropOffScript : MonoBehaviour
 {
     public GameObject player;
     public Transform holdPos;
