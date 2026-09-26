cbd8812d7c45eb3f2ce727207e95b62068128f77
davdthegoat <dawoodamir06@gmail.com>
2026-09-26 17:41:52 +0500

    Regular commit
    Committing because ThirdSpace guidelines said to commit changes frequently.
    Grid system is now operational, but not adapted to our needs fully yet. I've spammed many comments where I feel like relevant changes should/could be made

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -15,19 +15,26 @@ public class Grid
         this.height = height;
         this.cellSize = cellSize;
 
+        // Debug.Log(width + " " + height); Array working (verified)
         gridArray = new int[width, height];
 
         for (int x = 0; x < gridArray.GetLength(0); x++) //This here cycles through the first dimension or x-cordinate or "collumns" of the array, here 0 means the first dimension and 1 means 2nd dimension
         {
-            for (int y = 0; x < gridArray.GetLength(1); y++) 
+            for (int y = 0; y < gridArray.GetLength(1); y++) //This one is height, x is width.
             {
-                UtilsClass.CreateWorldText(gridArray[x, y].ToString(), null, GetWorldPosition(x, y), 20, Color.white, TextAnchor.MiddleCenter);
+                UtilsClass.CreateWorldText(gridArray[x, y].ToString(), null, GetWorldPosition(x, y) + new Vector3(cellSize,cellSize) * 0.5f, 20, Color.white, TextAnchor.MiddleCenter); //"GetWorldPosition(x, y) + new Vector3(cellSize,cellSize)" controls where in a grid are objects/text shown
+                //The following two lines are to help visualize what the grid looks like and are not necessary as our grid will be invisible in the final product.
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1),Color.white, 100f);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x+1, y), Color.white, 100f);
             }
         }
+        Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f); //Draw horizontal line from (0,maxHeight) - (maxWidth,maxHeight)
+        Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f); //Same thing but vertical
     }
 
     private Vector3 GetWorldPosition(int x, int y)
     {
-        return new Vector3(x, y) * cellSize; //What is x - y here?
+        return new Vector3(x, y) * cellSize; //I believe if we want to change where the grid is, for example the grid should span x-y or z-y, we change this here, as Vector3 has (x,y,z). Or we could try rotating through inspector
+        //Also, we could probably add slanted shelves like in the real olivander's store with this with 1,1,1.
     }
 }
--- Assets/Scripts/TestingGrid.cs
+++ Assets/Scripts/TestingGrid.cs
@@ -5,6 +5,6 @@ public class TestingGrid : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Grid grid = new Grid(4, 2, 10f); //the third parameter here is cellSize
+        Grid grid = new Grid(10, 10, 10f); //the third parameter here is cellSize, change it to change how many units apart each grid should be
     }
 }
